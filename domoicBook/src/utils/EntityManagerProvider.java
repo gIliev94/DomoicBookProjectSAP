1b0b22d8adb57ab943fc6055413f3c4bb16b41f9
Georgi Iliev <neokaozzz@gmail.com>
2016-02-03 21:56:26 +0200

    Utils adjustment

--- domoicBook/src/utils/EntityManagerProvider.java
+++ domoicBook/src/utils/EntityManagerProvider.java
@@ -3,10 +3,21 @@ package utils;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
+/**
+ * 
+ * @authors Georgi Iliev, Vencislav Penev
+ *
+ */
 public class EntityManagerProvider {
 
     private static final String PERSISTENCE_UNIT = "domoic-persistence-unit";
 
+    private EntityManagerProvider() {
+    }
+
+    /**
+     * @return
+     */
     public static EntityManager getEntityManager() {
 	return Persistence.createEntityManagerFactory(PERSISTENCE_UNIT).createEntityManager();
     }
